8604b930e5bc1fa26b2253c050acd0b69f46d71d
jisaamd <Abe.Mangona@jbhunt.com>
2019-04-05 11:14:02 -0500

    all

--- src/state/sagas/rootSaga.js
+++ src/state/sagas/rootSaga.js
@@ -1,15 +1,15 @@
 import { quoteSaga } from "./quote/getQuote.saga";
 import { gifSaga } from "./gif/getGif.saga";
-import fork from 'redux-saga/effects';
+import { fork, all } from 'redux-saga/effects'
 import { sagaMiddleware} from "../configureStore";
 
 function* rootSaga () {
-    yield [
+    yield all([
         fork(quoteSaga), // saga1 can also yield [ fork(actionOne), fork(actionTwo) ]
         fork(gifSaga),
-    ];
+    ]);
 }
 
 export default function startSaga () {
-    sagaMiddleware.run(rootSaga())
+    sagaMiddleware.run(rootSaga)
 }
